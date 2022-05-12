f6febd794493d15e025f4d6d12110e39c9668096
Nathaniel Johnston <j.johnston.tech@gmail.com>
2022-05-12 15:17:45 -0600

    Add mentor/mentees tests

--- api/src/tests/routes.test.js
+++ api/src/tests/routes.test.js
@@ -18,6 +18,30 @@ const exampleUser = {
   is_admin: false
 };
 let exampleUserResponse = null;
+const exampleUserID = 1;
+const exampleMentorData = {
+  user_id: 4,
+  mentor_id: 1
+};
+const exampleMentorDataUpdate = {
+  user_id: 4,
+  mentor_id: 2
+};
+
+/* Template:
+
+  test("", (done) => {
+    request(testApp)
+    // Method
+    .expect(response => {
+      // Test
+    })
+    .end((err, res) => {
+      if (err) throw err;
+      done();
+    })
+  });
+*/
 
 test('GETs Root Route', (done) => {
   request(testApp)
@@ -88,5 +112,70 @@ describe("Users Route:", () => {
         if (err) throw err;
         done();
       })
+  });
+});
+
+describe("User Mentorship Routes:", () => {
+  test("Retrieves mentees for a given user.", (done) => {
+    request(testApp)
+      .get(`/users/mentees/${exampleUserID}`)
+      .expect(response => {
+        expect(response.body[0].user_id).toBe(1);
+        expect(response.body[0].mentor_id).toBe(5);
+      })
+      .end((err, res) => {
+        if (err) throw err;
+        done();
+      })
+  });
+  test("Retrieves mentors for a given user.", (done) => {
+    request(testApp)
+    .get(`/users/mentors/${exampleUserID}`)
+    .expect(response => {
+      expect(response.body[0].user_id).toBe(2);
+      expect(response.body[0].mentor_id).toBe(1);
+    })
+    .end((err, res) => {
+      if (err) throw err;
+      done();
+    })
+  });
+  test("Adds a new mentor/mentee relationship.", (done) => {
+    request(testApp)
+    .post('/users/mentors')
+    .send(exampleMentorData)
+    .expect(response => {
+      expect(response.body.user_id).toBe(4);
+      expect(response.body.mentor_id).toBe(1);
+    })
+    .end((err, res) => {
+      if (err) throw err;
+      done();
+    })
+  });
+  test("Patches a mentor/mentee relationship.", (done) => {
+    request(testApp)
+    .patch(`/users/mentors/?user=${exampleMentorData.user_id}&mentor=${exampleMentorData.mentor_id}`)
+    .send(exampleMentorDataUpdate)
+    .expect(response => {
+      expect(response.body.user_id).toBe(4);
+      expect(response.body.mentor_id).toBe(2);
+    })
+    .end((err, res) => {
+      if (err) throw err;
+      done();
+    })
+  });
+  test("Removes a mentor/mentee relationship.", (done) => {
+    request(testApp)
+    .delete(`/users/mentors/?user=${exampleMentorData.user_id}&mentor=${exampleMentorDataUpdate.mentor_id}`)
+    .expect(response => {
+      expect(response.body.user_id).toBe(4);
+      expect(response.body.mentor_id).toBe(2);
+    })
+    .end((err, res) => {
+      if (err) throw err;
+      done();
+    })
   });
 })
