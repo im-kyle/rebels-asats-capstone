cc8186f5b9a4abf5f1c680d8923c479f12575023
Kyle Horne <kylehorne@outlook.com>
2022-05-17 12:14:49 -0700

    added awards for MTIs & MTLs

--- api/db/seeds/2_requirements.js
+++ api/db/seeds/2_requirements.js
@@ -9,16 +9,16 @@ exports.seed = async function(knex) {
   await knex('requirements').insert([
     //Rank Categories
     {afscs_code: null, rank_category: 'Junior Enlisted'}, //1
-    {afscs_code: null, rank_category: 'NCO'}, //2
-    {afscs_code: null, rank_category: 'SNCO'}, //3
-    {afscs_code: null, rank_category: 'CGO'}, //4
-    {afscs_code: null, rank_category: 'FGO'}, //5
+    {afscs_code: null, rank_category: 'NCO'}, //2 Non-Commissioned Officer
+    {afscs_code: null, rank_category: 'SNCO'}, //3 Senior NCO
+    {afscs_code: null, rank_category: 'CGO'}, //4 Company Grade Officer
+    {afscs_code: null, rank_category: 'FGO'}, //5 Field Grade Officer
     //AFSCS
-    {afscs_code: '8B000', rank_category: null}, //6
-    {afscs_code: '8B100', rank_category: null}, //7
-    {afscs_code: '8F000', rank_category: null}, //8
-    {afscs_code: '8G000', rank_category: null}, //9
-    {afscs_code: '9T200', rank_category: null}, //10
-    {afscs_code: '9T100', rank_category: null} //11
+    {afscs_code: '8B000', rank_category: null}, //6 MTI
+    {afscs_code: '8B100', rank_category: null}, //7 MTL
+    {afscs_code: '8F000', rank_category: null}, //8 First Sergeant
+    {afscs_code: '8G000', rank_category: null}, //9 Honor Guard
+    {afscs_code: '9T200', rank_category: null}, //10 Pre-Cadet Assigned
+    {afscs_code: '9T100', rank_category: null} //11 Officer Trainee
   ]);
 };
