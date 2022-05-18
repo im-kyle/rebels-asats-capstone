593d8eaf25cf88579acd8c03e3176e8e24f64c90
Nathaniel Johnston <j.johnston.tech@gmail.com>
2022-05-18 13:51:57 -0600

    Update UI integration tests

--- ui/cypress/integration/Home.spec.js
+++ ui/cypress/integration/Home.spec.js
@@ -5,7 +5,10 @@ describe("Home Page", ()=>{
     cy.visit('localhost:3000')
   })
   it("should show the home page",()=>{
-    cy.get("main").contains("Home")
+    cy.get("main").contains("Welcome to ASATS!")
+    cy.get("main").contains("Dashboard")
+    cy.get("main").contains("Awards")
+    cy.get("main").contains("Packages")
   })
 })
 
--- ui/cypress/integration/TopBar.spec.js
+++ ui/cypress/integration/TopBar.spec.js
@@ -1,11 +1,29 @@
 import '@testing-library/cypress/add-commands'
 
-describe("Home Page", ()=>{
+describe("Top Bar", ()=>{
   before(()=>{
     cy.visit('localhost:3000')
   })
   it("should show the Top Bar",()=>{
+    cy.get("header").contains("Dashboard")
+    cy.get("header").contains("Awards")
+    cy.get("header").contains("Packages")
+    cy.get("header").contains("ASATS")
+  })
+  it("should rotue to select pages when not signed in",()=>{
     cy.get("header").contains("Dashboard").click().wait(1000)
-    cy.get("main").contains("Dashboard")
+    cy.get("main").contains("Welcome to ASATS!")
+    cy.get("header").contains("Awards").click().wait(1000)
+    cy.get("main").contains("Awards")
+    cy.get("header").contains("Packages").click().wait(1000)
+    cy.get("main").contains("Awards")
+  })
+  it("should rotue to all pages when signed in",()=>{
+    cy.get("header").contains("Dashboard").click().wait(1000)
+    cy.get("main").contains("Welcome to ASATS!")
+    cy.get("header").contains("Awards").click().wait(1000)
+    cy.get("main").contains("Awards")
+    cy.get("header").contains("Packages").click().wait(1000)
+    cy.get("main").contains("Awards")
   })
 })
